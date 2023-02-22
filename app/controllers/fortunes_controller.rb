6c54278443414818c82bbb3f9bcf2cdb05c69c1f
Paulina Delmore <delmore.paulina@gmail.com>
2023-02-22 19:18:39 +0000

    passed!

--- app/controllers/fortunes_controller.rb
+++ app/controllers/fortunes_controller.rb
@@ -2,7 +2,23 @@ class FortunesController < ApplicationController
 
 def horoscopes
 
-@sign = params.fetch("the_sign")
+all_zodiacs = Zodiac.list
+
+@zodiac = params.fetch("the_sign").to_sym
+
+
+
+@horoscope = all_zodiacs.fetch(@zodiac).fetch(:horoscope)
+
+
+@array_of_numbers = Array.new
+
+5.times do
+  another_number = rand(1...100)
+  
+  @array_of_numbers.push(another_number)
+
+end
 
   render({ :template => "dice_templates/infinity_horoscopes.html.erb" })
 end
